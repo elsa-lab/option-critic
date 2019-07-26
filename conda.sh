0f4a09d6a80fdb84366d9310352f4ff01f9143a6
Shawn Chang <ebola777@yahoo.com.tw>
2019-07-26 17:11:41 +0800

    update scripts

---
+++ conda.sh
@@ -0,0 +1,8 @@
+# Create a Conda environment
+conda create -n option_critic python=2.7
+
+# Activate the Conda environment
+conda activate option_critic
+
+# Remove the Conda environment
+conda remove -n option_critic --all
---
+++ install.sh
@@ -0,0 +1,2 @@
+# You should install the packages in the activated Conda environment
+pip install -r requirements.txt
