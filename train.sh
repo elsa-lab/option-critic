31df8e7072a3aacb8f0fe116e145a735ae47657e
Shawn Chang <ebola777@yahoo.com.tw>
2019-07-26 17:54:49 +0800

    update scripts

--- train.sh
+++ train.sh
@@ -1,14 +1,14 @@
 # Train directly
-ENV_ID='beam_rider'; THEANO_FLAGS='floatX=float32' python train_q.py --rom $ENV_ID --num-options 8 --folder-name $ENV_ID
+ENV_ID='beam_rider'; THEANO_FLAGS='floatX=float32,device=cuda0,gpuarray.preallocate=1' python train_q.py --rom $ENV_ID --num-options 8 --folder-name $ENV_ID
 
 # Train in Tmux detached session
-ENV_ID='beam_rider'; tmux new -d -s "train/option_critic/env-id-$ENV_ID" "mkdir -p models/$ENV_ID; THEANO_FLAGS='floatX=float32' python train_q.py --rom=$ENV_ID --num-options=8 --folder-name=$ENV_ID > models/$ENV_ID/stdout.log 2> models/$ENV_ID/stderr.log"
-ENV_ID='breakout'; tmux new -d -s "train/option_critic/env-id-$ENV_ID" "mkdir -p models/$ENV_ID; THEANO_FLAGS='floatX=float32' python train_q.py --rom=$ENV_ID --num-options=8 --folder-name=$ENV_ID > models/$ENV_ID/stdout.log 2> models/$ENV_ID/stderr.log"
-ENV_ID='enduro'; tmux new -d -s "train/option_critic/env-id-$ENV_ID" "mkdir -p models/$ENV_ID; THEANO_FLAGS='floatX=float32' python train_q.py --rom=$ENV_ID --num-options=8 --folder-name=$ENV_ID > models/$ENV_ID/stdout.log 2> models/$ENV_ID/stderr.log"
-ENV_ID='pong'; tmux new -d -s "train/option_critic/env-id-$ENV_ID" "mkdir -p models/$ENV_ID; THEANO_FLAGS='floatX=float32' python train_q.py --rom=$ENV_ID --num-options=8 --folder-name=$ENV_ID > models/$ENV_ID/stdout.log 2> models/$ENV_ID/stderr.log"
-ENV_ID='qbert'; tmux new -d -s "train/option_critic/env-id-$ENV_ID" "mkdir -p models/$ENV_ID; THEANO_FLAGS='floatX=float32' python train_q.py --rom=$ENV_ID --num-options=8 --folder-name=$ENV_ID > models/$ENV_ID/stdout.log 2> models/$ENV_ID/stderr.log"
-ENV_ID='seaquest'; tmux new -d -s "train/option_critic/env-id-$ENV_ID" "mkdir -p models/$ENV_ID; THEANO_FLAGS='floatX=float32' python train_q.py --rom=$ENV_ID --num-options=8 --folder-name=$ENV_ID > models/$ENV_ID/stdout.log 2> models/$ENV_ID/stderr.log"
-ENV_ID='space_invaders'; tmux new -d -s "train/option_critic/env-id-$ENV_ID" "mkdir -p models/$ENV_ID; THEANO_FLAGS='floatX=float32' python train_q.py --rom=$ENV_ID --num-options=8 --folder-name=$ENV_ID > models/$ENV_ID/stdout.log 2> models/$ENV_ID/stderr.log"
+ENV_ID='beam_rider'; tmux new -d -s "train/option_critic/env-id-$ENV_ID" "mkdir -p models/$ENV_ID; THEANO_FLAGS='floatX=float32,device=cuda0,gpuarray.preallocate=1' python train_q.py --rom=$ENV_ID --num-options=8 --folder-name=$ENV_ID > models/$ENV_ID/stdout.log 2> models/$ENV_ID/stderr.log"
+ENV_ID='breakout'; tmux new -d -s "train/option_critic/env-id-$ENV_ID" "mkdir -p models/$ENV_ID; THEANO_FLAGS='floatX=float32,device=cuda0,gpuarray.preallocate=1' python train_q.py --rom=$ENV_ID --num-options=8 --folder-name=$ENV_ID > models/$ENV_ID/stdout.log 2> models/$ENV_ID/stderr.log"
+ENV_ID='enduro'; tmux new -d -s "train/option_critic/env-id-$ENV_ID" "mkdir -p models/$ENV_ID; THEANO_FLAGS='floatX=float32,device=cuda0,gpuarray.preallocate=1' python train_q.py --rom=$ENV_ID --num-options=8 --folder-name=$ENV_ID > models/$ENV_ID/stdout.log 2> models/$ENV_ID/stderr.log"
+ENV_ID='pong'; tmux new -d -s "train/option_critic/env-id-$ENV_ID" "mkdir -p models/$ENV_ID; THEANO_FLAGS='floatX=float32,device=cuda0,gpuarray.preallocate=1' python train_q.py --rom=$ENV_ID --num-options=8 --folder-name=$ENV_ID > models/$ENV_ID/stdout.log 2> models/$ENV_ID/stderr.log"
+ENV_ID='qbert'; tmux new -d -s "train/option_critic/env-id-$ENV_ID" "mkdir -p models/$ENV_ID; THEANO_FLAGS='floatX=float32,device=cuda0,gpuarray.preallocate=1' python train_q.py --rom=$ENV_ID --num-options=8 --folder-name=$ENV_ID > models/$ENV_ID/stdout.log 2> models/$ENV_ID/stderr.log"
+ENV_ID='seaquest'; tmux new -d -s "train/option_critic/env-id-$ENV_ID" "mkdir -p models/$ENV_ID; THEANO_FLAGS='floatX=float32,device=cuda0,gpuarray.preallocate=1' python train_q.py --rom=$ENV_ID --num-options=8 --folder-name=$ENV_ID > models/$ENV_ID/stdout.log 2> models/$ENV_ID/stderr.log"
+ENV_ID='space_invaders'; tmux new -d -s "train/option_critic/env-id-$ENV_ID" "mkdir -p models/$ENV_ID; THEANO_FLAGS='floatX=float32,device=cuda0,gpuarray.preallocate=1' python train_q.py --rom=$ENV_ID --num-options=8 --folder-name=$ENV_ID > models/$ENV_ID/stdout.log 2> models/$ENV_ID/stderr.log"
 
 # Kill the Tmux detached session
 ENV_ID='beam_rider'; tmux kill-ses -t "train/option_critic/env-id-$ENV_ID"
