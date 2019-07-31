896733eeb1bed39b18291413acfe515eb4674b83
Shawn Chang <ebola777@yahoo.com.tw>
2019-08-01 01:54:06 +0800

    make it working again

--- run_best_model.py
+++ run_best_model.py
@@ -1,12 +1,21 @@
-import subprocess, sys
+import os
+import subprocess
+import sys
 
-def run_watch():
-  command = ['python', 'train_q.py', '--steps-per-epoch', '0',
-  '--test-length', '100000', '--nn-file', sys.argv[1], '--display-screen',
-  '--max-history', '10', '--testing'] + sys.argv[2:]
 
-  p1 = subprocess.Popen(command)
-  p1.wait()
-    
+def test():
+    command = ['python',
+               'train_q.py',
+               '--steps-per-epoch', '0',
+               '--test-length', '100000',
+               '--nn-file', sys.argv[1],
+               '--max-history', '10',
+               '--testing'] + sys.argv[2:]
+
+    p1 = subprocess.Popen(command, env=dict(
+        os.environ, THEANO_FLAGS='floatX=float32,device=gpu'))
+    p1.wait()
+
+
 if __name__ == "__main__":
-  run_watch()
+    test()
