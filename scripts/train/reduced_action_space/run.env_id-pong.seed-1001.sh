675a418e205a6f0deaef65648e2493bd9de78997
Shawn Chang <ebola777@yahoo.com.tw>
2019-10-08 14:55:16 +0800

    add reduced_action_space scripts and specs

---
+++ scripts/train/reduced_action_space/run.env_id-pong.seed-1001.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Cause the script to exit on any errors
+# Reference: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euo pipefail
+
+################################################################################
+# Set Names
+################################################################################
+
+# Set the name
+export NAME="reduced_action_space"
+
+################################################################################
+# Set Variable Training Arguments
+################################################################################
+
+# Set the environment ID
+export ENV_ID="pong"
+
+# Set the seed
+export SEED=1001
+
+################################################################################
+# Set Fixed Training Arguments
+################################################################################
+
+# Set the number of epochs
+export EPOCHS=400
+
+# Set the number of options
+export NUM_OPTIONS=8
+
+################################################################################
+# Initialization
+################################################################################
+
+# Set the common variables
+source "scripts/.common/set_common_variables.sh"
+
+# Initialize the training variables
+source "scripts/train/.common/init_training_variables.sh"
+
+# Initialize the training environment
+source "scripts/train/.common/init_training_environment.sh"
+
+################################################################################
+# Start Training
+################################################################################
+
+# Run the common script
+bash "scripts/train/reduced_action_space/.common/run.sh"
