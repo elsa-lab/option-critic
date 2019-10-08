c5c5cabd927d58a9c679c8d1acb01ca99edc0dbd
Shawn Chang <ebola777@yahoo.com.tw>
2019-10-08 14:57:46 +0800

    add reduced_action_space scripts

---
+++ scripts/noodles/reduced_action_space/purge.env_id-asteroids.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Cause the script to exit on any errors
+# Reference: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euo pipefail
+
+################################################################################
+# Set Actions
+################################################################################
+
+# Set the Noodles action
+export NOODLES_ACTION=purge
+
+################################################################################
+# Set Paths
+################################################################################
+
+# Set the path of the spec
+export SPEC_PATH='specs/train/reduced_action_space/env_id-asteroids.yml'
+
+################################################################################
+# Start Training
+################################################################################
+
+# Run the common script
+bash "scripts/noodles/.common/$NOODLES_ACTION.sh"
