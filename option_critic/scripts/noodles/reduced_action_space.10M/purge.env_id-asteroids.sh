ed975687433e8246e6d90b8d61290d1b16623e67
Shawn Chang <ebola777@yahoo.com.tw>
2019-11-02 14:51:56 +0800

    add 10M scripts and specs

---
+++ option_critic/scripts/noodles/reduced_action_space.10M/purge.env_id-asteroids.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Cause the script to exit on any errors
+# Reference: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euo pipefail
+
+################################################################################
+# Set Actions
+################################################################################
+
+# Set the Noodles action
+export NOODLES_ACTION="purge"
+
+################################################################################
+# Set Paths
+################################################################################
+
+# Set the path of the spec
+export SPEC_PATH='option_critic/specs/train/reduced_action_space.10M/env_id-asteroids.yml'
+
+################################################################################
+# Start Training
+################################################################################
+
+# Run the common script
+bash "option_critic/scripts/noodles/_common/$NOODLES_ACTION.sh"
---
+++ option_critic/scripts/train/reduced_action_space.10M/run.env_id-enduro.seed-1004.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Cause the script to exit on any errors
+# Reference: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euo pipefail
+
+################################################################################
+# Set Names
+################################################################################
+
+# Set the name
+export NAME="reduced_action_space.10M"
+
+################################################################################
+# Set Variable Training Arguments
+################################################################################
+
+# Set the environment ID
+export ENV_ID="enduro"
+
+# Set the seed
+export SEED=1004
+
+################################################################################
+# Run the Common Script
+################################################################################
+
+# Run the common script
+bash "option_critic/scripts/train/$NAME/_common/run.sh"
